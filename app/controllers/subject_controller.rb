0cfecfd03705141425ae994a9ce64655e997d2fb
Julian Rosenthal <julianrosenthal@yahoo.com>
2021-01-07 15:09:45 -0700

    added slug methods

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -5,4 +5,15 @@ class SubjectController < ApplicationController
             erb :'subjects/index'
         end
     end
+
+    get '/subjects/:slug' do
+        if logged_in? 
+            @subject = current_user.subjects.find_by_slug(params[:slug])
+            if @subject
+                erb :'subjects/'
+            end
+        end
+    end
+
+
 end
