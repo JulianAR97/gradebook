22e32344e3ac55ac20043fe9fe83bfb04496b98d
Julian Rosenthal <julianrosenthal@yahoo.com>
2021-01-07 15:10:23 -0700

    added slug functions

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,12 @@
 class Subject < ActiveRecord::Base
     belongs_to :user
     has_many :assignments
+
+    def self.find_by_slug(slug)
+        all.find { |s| s.slugify == slug }
+    end
+
+    def slugify
+        title.downcase.gsub(' ', '-')
+    end
 end
