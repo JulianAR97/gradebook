e9a935e9c05ff1011439d07ad1d8135f481463af
Julian Rosenthal <julianrosenthal@yahoo.com>
2021-01-07 12:09:42 -0700

    added user_subjects migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_030102) do
+ActiveRecord::Schema.define(version: 2021_01_07_190723) do
 
   create_table "assignments", force: :cascade do |t|
     t.string "category"
     t.integer "score_earned"
     t.integer "score_possible"
     t.integer "user_id"
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "title"
+  end
+
+  create_table "user_subjects", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "subject_id"
   end
 
   create_table "users", force: :cascade do |t|
