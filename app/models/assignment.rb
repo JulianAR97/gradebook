2eccce5b80e03501d31139c1bfcd0e70250cc0a2
Julian Rosenthal <julianrosenthal@yahoo.com>
2021-01-08 09:04:33 -0700

    added function to sort assignments for table

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,8 +1,17 @@
 class Assignment < ActiveRecord::Base
     belongs_to :subject
 
+    # Working, but ugly, fix please
     def self.score_as_percentage(score_earned, score_possible)
         "%.2f" % ((score_earned * 0.1) / (score_possible * 0.1) * 100) + "%"
     end
 
+    def self.sort_for_table
+        all.sort_by { |a| [a[:category], a[:date]] }
+    end
+
+    def display_date
+        # Date is written as 2020-12-08, and I don't care about the year for display
+        date.split('-')[1..2].join('/')
+    end
 end
