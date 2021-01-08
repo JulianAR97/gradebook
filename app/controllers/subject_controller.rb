344706ca48ee1b934402067d3af6ecbe3024f610
Julian Rosenthal <julianrosenthal@yahoo.com>
2021-01-07 19:55:10 -0700

    added redirection to /subjets

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -3,7 +3,7 @@ class SubjectController < ApplicationController
         if logged_in?
             @subjects = current_user.subjects
             erb :'subjects/index'
-        else 
+        else
             redirect '/login'
         end
     end
@@ -11,6 +11,8 @@ class SubjectController < ApplicationController
     get '/subjects/new' do
         if logged_in?
             erb :'subjects/new'
+        else
+            redirect '/login'
         end
     end
 
@@ -28,7 +30,19 @@ class SubjectController < ApplicationController
     end
 
     post '/subjects' do
-
+        if logged_in?
+            if params[:title] == ''
+                # Display message class creation failed, field cannot be empty
+                redirect '/subjects'
+            elsif current_user.subjects.find_by(title: params[:title])
+                # Display message that cannot create class with same name
+                redirect '/subjects/new'
+            else
+                @subject = current_user.subjects.build(title: params[:title])
+                redirect "/subjects/#{@subject.slugify}" if @subject.save
+            end
+        else
+            redirect '/login'
+        end
     end
-
 end
