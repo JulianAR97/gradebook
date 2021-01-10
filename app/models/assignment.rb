b97183f5270034e560e52b9b26cb448238be1b81
Julian Rosenthal <julianrosenthal@yahoo.com>
2021-01-10 12:12:53 -0700

    removed format date for sort method

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -6,15 +6,7 @@ class Assignment < ActiveRecord::Base
         "%.2f" % ((score_earned * 0.1) / (score_possible * 0.1) * 100) + "%"
     end
 
-    def self.f_date_for_sort(date)
-        Date.strptime(date, '%m/%d/%Y').strftime('%Y/%m/%d')
-    end
-
     def self.sort_for_table
-        all.sort_by { |a| [a[:category], f_date_for_sort(a[:date])] }
+        all.sort_by { |a| [a[:category], a[:date]] }
     end
-
-    # Only want this called when sorting for table
-    private_class_method :f_date_for_sort
-
 end
