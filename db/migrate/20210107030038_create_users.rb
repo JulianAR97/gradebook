f9519b2de0e409dad3c4534d18360b8292141270
Julian Rosenthal <julianrosenthal@yahoo.com>
2021-01-10 14:57:39 -0700

    removed comments

--- db/migrate/20210107030038_create_users.rb
+++ db/migrate/20210107030038_create_users.rb
@@ -3,8 +3,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-
-      # How do we seed passwords with password_digest
     end
   end
 end
