3e5bb11a26d3ca3678e000d9028b1c2cf7d195c2
Julian <julianrosenthal@yahoo.com>
2021-01-18 19:45:14 -0700

    validates password length

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,17 @@
 class UserController < ApplicationController
+    before '/login' || '/signup' do
+        redirect '/subjects' if logged_in?
+    end
+
     get '/login' do
-        if !logged_in?
-            erb :'users/login'
-        else
-            redirect '/subjects'
-        end
+        @user = User.new
+        erb :'users/login'
     end
 
     post '/login' do
         @user = User.find_by(username: params[:username])
 
-        # Use && user.authenticate(params[:password]) with bcrypt
+        # Same as if @user and @user.authenticate
         if @user&.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect '/subjects'
@@ -21,21 +22,19 @@ class UserController < ApplicationController
     end
 
     get '/signup' do
-        if !logged_in?
-            @user = User.new
-            erb :'users/signup'
-        else
-            redirect '/subjects'
-        end
+        @user = User.new
+        erb :'users/signup'
     end
 
     post '/signup' do
         @user = User.new(username: params[:username], password: params[:password])
+        # Can I just do @user.save?
         if @user.valid?
             @user.save
             session[:user_id] = @user.id
             redirect '/subjects'
         else
+            # Render instead of redirect to get error messages
             erb :'users/signup'
         end
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
     has_many :subjects
     has_secure_password
     validates :username, presence: true, uniqueness: true
-
+    validates :password, presence: true, length: { within: 8..40 }
 end
